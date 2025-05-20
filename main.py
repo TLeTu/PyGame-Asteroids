5963a77f6587dacf2189e4afd2248395e11feab1
TLeTu <tranletu2003@gmail.com>
2025-05-20 19:49:53 +0700

    Draw screen

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pygame
+from constants import *
+
+
+def update(screen):
+    while True:
+        # Fill it with black
+        screen.fill((255, 255, 255), rect = None, special_flags = 0)
+        pygame.display.flip()
+
+
+def main():
+    # Initialize pygame
+    pygame.init()
+
+    # Setup the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    update(screen)
+    
 
 if __name__ == "__main__":
     main()
