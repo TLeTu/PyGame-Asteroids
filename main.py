41ffc321b4d905964c0243b325b249a1823e873e
TLeTu <tranletu2003@gmail.com>
2025-05-20 19:53:25 +0700

    Check window close

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from constants import *
 
 def update(screen):
     while True:
+        # Check if the the window have closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         # Fill it with black
         screen.fill((255, 255, 255), rect = None, special_flags = 0)
         pygame.display.flip()
