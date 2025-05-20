82d4d0b252e344be95e6520a666fada648abe760
TLeTu <tranletu2003@gmail.com>
2025-05-20 20:49:18 +0700

    FPS

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ import pygame
 from constants import *
 
 
-def update(screen):
+
+def update(screen, clock, delta):
     while True:
         # Check if the the window have closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # Fill it with black
+        # Fill it with white
         screen.fill((255, 255, 255), rect = None, special_flags = 0)
         pygame.display.flip()
+
+        # Set FPS
+        delta[0] = clock.tick(60) / 1000 # 60 FPS
 
 
 def main():
     # Initialize pygame
     pygame.init()
+    # Setup FPS
+    pyClock = pygame.time.Clock()
+    delta = [0]
 
     # Setup the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Game loop
-    update(screen)
+    update(screen, pyClock, delta)
     
 
 if __name__ == "__main__":
