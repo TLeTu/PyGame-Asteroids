f4c05ec644a938f8b3ce554939b6dbbd3e628f21
TLeTu <tranletu2003@gmail.com>
2025-05-21 14:06:22 +0700

    Add player

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
-from circleshape import *
+from objects.circleshape import *
+from objects.player import *
 
 
 
-def update(screen, clock, delta):
+def update(screen, clock, delta, player):
     while True:
         # Check if the the window have closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # Fill it with white
-        screen.fill((255, 255, 255), rect = None, special_flags = 0)
+        # Fill it with black
+        screen.fill((0, 0, 0), rect = None, special_flags = 0)
         pygame.display.flip()
+
+        # Draw the player
+        player.draw(screen)
 
         # Set FPS
         delta[0] = clock.tick(60) / 1000 # 60 FPS
@@ -29,8 +33,13 @@ def main():
     # Setup the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Initialize player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = player(x, y)
+
     # Game loop
-    update(screen, pyClock, delta)
+    update(screen, pyClock, delta, player)
     
 
 if __name__ == "__main__":
